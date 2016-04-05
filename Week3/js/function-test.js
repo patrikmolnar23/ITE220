dab6823cc5478f70b424ad8e2d013e86fb01c45c
PatrikMolnar <molnarpatrik20@gmail.com>
2016-04-05 15:47:07 +0700

    new

---
+++ Week3/js/function-test.js
@@ -0,0 +1,8 @@
+var msg = "Sign up to recieve our newsletter for 10% off.";
+
+function updateMassage() {
+    var el = document.getElementById("heading-massage");
+    el.textContent = msg;
+}
+
+updateMassage();
