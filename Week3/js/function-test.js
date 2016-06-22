c717be860f0543b9f2ba9543af3f8421d9570dc6
Patrik Molnar <patrikmolnar22@gmail.com>
2016-06-22 19:55:00 +0700

    assignments
    late assignments

--- Week3/js/function-test.js
+++
@@ -1,8 +0,0 @@
-var msg = "Sign up to recieve our newsletter for 10% off.";
-
-function updateMassage() {
-    var el = document.getElementById("heading-massage");
-    el.textContent = msg;
-}
-
-updateMassage();
